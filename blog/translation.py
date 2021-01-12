7dbc2dfb3893ecee667ca0a1b1e28b75d0054fd4
Juan Ponce <juan@jmponce.com>
2021-01-12 13:50:17 +0000

    Corrected translation.py

--- blog/translation.py
+++ blog/translation.py
@@ -1,5 +1,8 @@
 from .models import BlogIndexPage, BlogPage
 
+from wagtail_modeltranslation.translation import TranslationOptions
+from modeltranslation.decorators import register
+
 @register(BlogIndexPage)
 class BlogIndexPageTR(TranslationOptions):
     fields = (
