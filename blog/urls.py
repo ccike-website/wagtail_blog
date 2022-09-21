2c60dfd21684b49e47e27a26d5b34f87ddd9bfbe
Alex Hayward <alex.hayward@telespazio.com>
2022-09-21 16:12:21 +0100

    Django 4 migration for path() vs url()

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import path_re
 from . import views
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    url(r'^tag/(?P<tag>[-\w]+)/', views.tag_view, name="tag"),
-    url(r'^category/(?P<category>[-\w]+)/feed/$', views.LatestCategoryFeed(), name="category_feed"),
-    url(r'^category/(?P<category>[-\w]+)/', views.category_view, name="category"),
-    url(r'^author/(?P<author>[-\w]+)/', views.author_view, name="author"),
-    url(r'(?P<blog_slug>[\w-]+)/rss.*/',
+    path_re(r'^tag/(?P<tag>[-\w]+)/', views.tag_view, name="tag"),
+    path_re(r'^category/(?P<category>[-\w]+)/feed/$', views.LatestCategoryFeed(), name="category_feed"),
+    path_re(r'^category/(?P<category>[-\w]+)/', views.category_view, name="category"),
+    path_re(r'^author/(?P<author>[-\w]+)/', views.author_view, name="author"),
+    path_re(r'(?P<blog_slug>[\w-]+)/rss.*/',
         views.LatestEntriesFeed(),
         name="latest_entries_feed"),
-    url(r'(?P<blog_slug>[\w-]+)/atom.*/',
+    path_re(r'(?P<blog_slug>[\w-]+)/atom.*/',
         views.LatestEntriesFeedAtom(),
         name="latest_entries_feed_atom"),
 ]
