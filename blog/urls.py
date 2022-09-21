6d569ce1e510ecf2570f17e3dfb2b6b536d3d5a9
Alex Hayward <alex.hayward@telespazio.com>
2022-09-21 16:22:17 +0100

    re_path not path_re

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path_re
+from django.urls import re_path
 from . import views
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path_re(r'^tag/(?P<tag>[-\w]+)/', views.tag_view, name="tag"),
-    path_re(r'^category/(?P<category>[-\w]+)/feed/$', views.LatestCategoryFeed(), name="category_feed"),
-    path_re(r'^category/(?P<category>[-\w]+)/', views.category_view, name="category"),
-    path_re(r'^author/(?P<author>[-\w]+)/', views.author_view, name="author"),
-    path_re(r'(?P<blog_slug>[\w-]+)/rss.*/',
+    re_path(r'^tag/(?P<tag>[-\w]+)/', views.tag_view, name="tag"),
+    re_path(r'^category/(?P<category>[-\w]+)/feed/$', views.LatestCategoryFeed(), name="category_feed"),
+    re_path(r'^category/(?P<category>[-\w]+)/', views.category_view, name="category"),
+    re_path(r'^author/(?P<author>[-\w]+)/', views.author_view, name="author"),
+    re_path(r'(?P<blog_slug>[\w-]+)/rss.*/',
         views.LatestEntriesFeed(),
         name="latest_entries_feed"),
-    path_re(r'(?P<blog_slug>[\w-]+)/atom.*/',
+    re_path(r'(?P<blog_slug>[\w-]+)/atom.*/',
         views.LatestEntriesFeedAtom(),
         name="latest_entries_feed_atom"),
 ]
